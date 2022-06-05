06a3e0ceb3b29c6f0b732e59ce01df7e094ad7c5
Nick Donaldson <ndonald2@gmail.com>
2022-06-05 14:27:25 -0600

    Move audio script and scene assets

--- global/Audio.gd
+++ global/Audio.gd
@@ -1,13 +1,18 @@
 extends Node
 
 const _FADE_RATE = 1.05
+const _INITIAL_FADEIN_CUTOFF = 250.0
 const _TARGET_FADEIN_CUTOFF = 20000.0
 
 func fade_in_scene():
 	var scene_bus_idx = AudioServer.get_bus_index("Scene")
 	var lpf = AudioServer.get_bus_effect(scene_bus_idx, 0) as AudioEffectFilter
 	
-	var cutoff = lpf.cutoff_hz
+	var cutoff = _INITIAL_FADEIN_CUTOFF
+	lpf.set_cutoff(cutoff)
+	
+	$Ambience.play()
+	
 	while cutoff < _TARGET_FADEIN_CUTOFF:
 		cutoff = min(_TARGET_FADEIN_CUTOFF, cutoff * _FADE_RATE)
 		lpf.set_cutoff(cutoff)
